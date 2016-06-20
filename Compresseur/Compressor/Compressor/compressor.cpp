7ba35d24743ca4bf3f3375c6bcfdf92ec78f12fc
Ugo <ugo.blanquet@viacesi.fr>
2016-06-20 09:41:17 +0200

    Working json reading+parsing

--- Compresseur/Compressor/Compressor/compressor.cpp
+++ Compresseur/Compressor/Compressor/compressor.cpp
@@ -136,61 +136,20 @@ QStringList Compressor::fuseFiles()
 			break;
 
 		QFile file(currentDir.absoluteFilePath(foundFiles[i]->text()));
-		QString val;
+		//QString val;
+		QByteArray jsonData;// = file.readAll();
 
 		if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {//reading file
-			val = file.readAll();
+			jsonData = file.readAll();
 			file.close();
-			qWarning() << val;
-			QJsonDocument d = QJsonDocument::fromJson(val.toUtf8());
-
-			QJsonObject jsonObject = d.object();
-			//jsonObject["DATA"].
-			QJsonArray jsonArray = jsonObject["DATA"].toArray();
-			QString first = jsonArray.first().toString();
-			//QString test2 = jsonArray[0].toString();
-			QStringList keys = jsonObject.keys();
-
-			qWarning() << jsonArray[0].toString();
-
-			/*totest
 			QJsonDocument document = QJsonDocument::fromJson(jsonData);
-    QJsonObject object = document.object();
+			QJsonObject object = document.object();
 
-    QJsonValue value = object.value("agentsArray");
-    QJsonArray array = value.toArray();
-    foreach (const QJsonValue & v, array)
-        qDebug() << v.toObject().value("ID").toInt();
-
-			*/
-			//fuck off under -----
-			QJsonObject sett2 = d.object();
-			QJsonValue value = sett2.value(QString("appName"));
-			qWarning() << value;
-			QJsonObject item = value.toObject();
-			qWarning() << tr("QJsonObject of description: ") << item;
-
-			/* incase of string value get value and convert into string*/
-			qWarning() << tr("QJsonObject[appName] of description: ") << item["description"];
-			QJsonValue subobj = item["description"];
-			qWarning() << subobj.toString();
-
-			/* incase of array get array and convert into string*/
-			qWarning() << tr("QJsonObject[appName] of value: ") << item["imp"];
-			QJsonArray test = item["imp"].toArray();
-			qWarning() << test[1].toString();
-
-			/*QString line;
-			QTextStream in(&file);
-			while (!in.atEnd()) {
-				if (progressDialog.wasCanceled())
-					break;
-				line = in.readLine();
-				if (line.contains(text)) {
-					foundFiles << files[i];
-					break;
-				}
-			}*/
+			QJsonValue value = object.value("DATA");
+			QJsonArray array = value.toArray();
+			foreach(const QJsonValue & v, array)
+				qDebug() << v.toObject().value("id").toString();
+			
 		}
 	}
 
